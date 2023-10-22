816b64b11dd48278f5b40388fa2b6b75387d1d20
armalunar <147956486+armalunar@users.noreply.github.com>
2023-10-22 11:05:46 -0300

    main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ bot = commands.Bot(command_prefix='.', intents=intents)
 conn = sqlite3.connect('tickets.db')
 c = conn.cursor()
 
-c.execute('''CREATE TABLE IF NOT EXISTS tickets (id INTEGER PRIMARY KEY, service_name TEXT, price REAL, responsible TEXT, user TEXT, notes TEXT, generated_at DATETIME)''')
+c.execute('''CREATE TABLE IF NOT EXISTS tickets (id INTEGER PRIMARY KEY, service_name TEXT, price REAL, responsible TEXT, user TEXT, email TEXT, notes TEXT, generated_at DATETIME)''')
 conn.commit()
 
 ticket_info = {}
@@ -47,12 +47,17 @@ async def start_ticket(ctx):
         msg = await bot.wait_for('message', check=check, timeout=60)
         ticket_info['responsible'] = msg.content
 
-        embed.set_field_at(0, name="Passo 4", value="Digite o nome do usuário:")
+        embed.set_field_at(0, name="Passo 4", value="Digite o nome do cliente:")
         await ctx.send(embed=embed)
         msg = await bot.wait_for('message', check=check, timeout=60)
         ticket_info['user'] = msg.content
 
-        embed.set_field_at(0, name="NOTAS", value="Digite suas observações (opcional):")
+        embed.set_field_at(0, name="Passo 5", value="Digite o email:")
+        await ctx.send(embed=embed)
+        msg = await bot.wait_for('message', check=check, timeout=60)
+        ticket_info['email'] = msg.content
+
+        embed.set_field_at(0, name="Passo 6", value="Digite suas observações (opcional):")
         await ctx.send(embed=embed)
         msg = await bot.wait_for('message', check=check, timeout=60)
         ticket_info['notes'] = msg.content
@@ -70,35 +75,44 @@ def generate_ticket_id():
         return 1
 
 async def generate_and_send_ticket(ctx):
-    ticket_image = Image.new('RGB', (600, 750), color='#e9e5cd')
+    ticket_image = Image.new('RGB', (600, 800), color='#e9e5cd')
     draw = ImageDraw.Draw(ticket_image)
 
     font_size = 20
     font = ImageFont.truetype("cour.ttf", font_size)
 
-    draw.text((10, 10), f'ID: {ticket_info["id"]}', fill='black', font=font)
-    draw.text((10, 40), f'Serviço: {ticket_info["service_name"]}', fill='black', font=font)
-    draw.text((10, 70), f'Preço: ${ticket_info["price"]:.2f}', fill='black', font=font)
-    draw.text((10, 100), f'Reponsável: {ticket_info["responsible"]}', fill='black', font=font)
-    draw.text((10, 130), f'Usuário: {ticket_info["user"]}', fill='black', font=font)
+    draw.text((10, 10), f'TICKET FISCAL', fill='black', font=font)
 
-    # Adicione o traço separador abaixo dos dados do usuário
-    draw.line([(10, 160), (590, 160)], fill='black', width=2)
+    y_offset = 40
+    draw.text((10, y_offset), f'ID: {ticket_info["id"]}', fill='black', font=font)
+    y_offset += 30
+    draw.text((10, y_offset), f'Serviço: {ticket_info["service_name"]}', fill='black', font=font)
+    y_offset += 30
+    draw.text((10, y_offset), f'Preço: ${ticket_info["price"]:.2f}', fill='black', font=font)
+    y_offset += 30
+    draw.text((10, y_offset), f'Reponsável: {ticket_info["responsible"]}', fill='black', font=font)
+    y_offset += 30
+    draw.text((10, y_offset), f'Usuário: {ticket_info["user"]}', fill='black', font=font)
+    y_offset += 30
+    draw.text((10, y_offset), f'Email: {ticket_info["email"]}', fill='black', font=font)
+    y_offset += 40
 
-    # Check if 'notes' key exists in ticket_info
     if 'notes' in ticket_info:
-        draw.text((10, 170), 'NOTAS:', fill='black', font=font)
-        draw.text((10, 200), ticket_info['notes'], fill='black', font=font)
+        draw.text((10, y_offset), 'NOTAS:', fill='black', font=font)
+        y_offset += 30
+        notes = ticket_info['notes'].split('\n')
+        for note in notes:
+            draw.text((10, y_offset), note, fill='black', font=font)
+            y_offset += 30
 
-    # Adicionar a hora exata que o ticket foi gerado
-    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    draw.text((10, 220), f'Gerado em: {current_time}', fill='black', font=font)
+    current_time = datetime.now().strftime("%Y-%m-%d")
+    draw.text((10, 670), f'Gerado em: {current_time}', fill='black', font=font)
 
     phrase = "TL STORE systems"
-    draw.text((10, 650), phrase, fill='black', font=font)
+    draw.text((10, 770), phrase, fill='black', font=font)
 
-    c.execute("INSERT INTO tickets (id, service_name, price, responsible, user, notes, generated_at) VALUES (?, ?, ?, ?, ?, ?, ?)",
-              (ticket_info["id"], ticket_info["service_name"], ticket_info["price"], ticket_info["responsible"], ticket_info["user"], ticket_info.get('notes', ''), current_time))
+    c.execute("INSERT INTO tickets (id, service_name, price, responsible, user, email, notes, generated_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+              (ticket_info["id"], ticket_info["service_name"], ticket_info["price"], ticket_info["responsible"], ticket_info["user"], ticket_info["email"], ticket_info.get('notes', ''), current_time))
     conn.commit()
 
     image_buffer = io.BytesIO()
@@ -111,20 +125,59 @@ async def generate_and_send_ticket(ctx):
 
 @bot.command(name='tickets')
 async def list_tickets(ctx):
-    c.execute("SELECT id, service_name, price, responsible, user, notes, generated_at FROM tickets")
+    c.execute("SELECT id, service_name, price, responsible, user, email, notes, generated_at FROM tickets")
     tickets = c.fetchall()
 
     if not tickets:
         await ctx.send("Nenhum ticket foi gerado ainda.")
     else:
-        message = "Lista de Tickets:\n"
         for ticket in tickets:
-            message += f"ID: {ticket[0]}, Serviço: {ticket[1]}, Preço: ${ticket[2]:.2f}, Responsável: {ticket[3]}, Usuário: {ticket[4]}\n"
-            if ticket[5]:  # Check if 'notes' exist
-                message += f"NOTAS: {ticket[5]}\n"
-            message += f"Gerado em: {ticket[6]}\n"
+            await send_ticket_image(ctx, ticket)
 
-        await ctx.send(message)
+async def send_ticket_image(ctx, ticket):
+    ticket_image = Image.new('RGB', (600, 800), color='#e9e5cd')
+    draw = ImageDraw.Draw(ticket_image)
+
+    font_size = 20
+    font = ImageFont.truetype("cour.ttf", font_size)
+
+    draw.text((10, 10), f'TICKET FISCAL', fill='black', font=font)
+
+    y_offset = 40
+    draw.text((10, y_offset), f'ID: {ticket[0]}', fill='black', font=font)
+    y_offset += 30
+    draw.text((10, y_offset), f'Serviço: {ticket[1]}', fill='black', font=font)
+    y_offset += 30
+    draw.text((10, y_offset), f'Preço: ${ticket[2]:.2f}', fill='black', font=font)
+    y_offset += 30
+    draw.text((10, y_offset), f'Reponsável: {ticket[3]}', fill='black', font=font)
+    y_offset += 30
+    draw.text((10, y_offset), f'Usuário: {ticket[4]}', fill='black', font=font)
+    y_offset += 30
+    draw.text((10, y_offset), f'Email: {ticket[5]}', fill='black', font=font)
+    y_offset += 40
+
+    if ticket[6]:
+        draw.text((10, y_offset), 'NOTAS:', fill='black', font=font)
+        y_offset += 30
+        notes = ticket[6].split('\n')
+        for note in notes:
+            draw.text((10, y_offset), note, fill='black', font=font)
+            y_offset += 30
+
+    current_time = ticket[7][:10]
+    draw.text((10, 670), f'Gerado em: {current_time}', fill='black', font=font)
+
+    phrase = "TL STORE systems"
+    draw.text((10, 770), phrase, fill='black', font=font)
+
+    image_buffer = io.BytesIO()
+    ticket_image.save(image_buffer, format='PNG')
+    image_buffer.seek(0)
+
+    embed = discord.Embed(title=f"Ticket Fiscal #{ticket[0]}", color=discord.Colour(0xFFFFFF))
+    embed.set_image(url="attachment://ticket.png")
+    await ctx.send(embed=embed, file=discord.File(image_buffer, filename='ticket.png'))
 
 @bot.command(name='cleartickets')
 async def clear_tickets(ctx):
@@ -132,4 +185,4 @@ async def clear_tickets(ctx):
     conn.commit()
     await ctx.send("Todos os tickets foram excluídos com sucesso.")
 
-bot.run('seu token aqui')
+bot.run('SEU_TOKEN_AQUI')
